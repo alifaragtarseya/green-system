44d62e3f3574701abc4901c1ef6a0a96f7f14c3b
AhmedRamadanTarseya <73784906+AhmedRamadan03@users.noreply.github.com>
2023-10-23 14:32:14 +0300

    update services

--- resources/views/front/home/section_services.blade.php
+++ resources/views/front/home/section_services.blade.php
@@ -36,10 +36,11 @@
                                 <p>
                                     <b class="main-color">
                                         <a href="{{ route('front.services') }}" style="color: #F7A11B">{{ __('lang.read_more') }}
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="11" viewBox="0 0 14 11" fill="none">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 11" fill="none">
                                                 <path d="M10.9151 5.364L6.74295 1.19181L7.93477 -2.34468e-07L13.2988 5.364L7.93477 10.728L6.74295 9.53534L10.9151 5.36316L10.9151 5.364Z" fill="#F7A11B"/>
                                                 <path d="M4.17219 5.364L4.23944e-06 1.19181L1.19182 -2.34468e-07L6.55582 5.364L1.19182 10.728L3.87473e-06 9.53534L4.17219 5.36316L4.17219 5.364Z" fill="#F7A11B"/>
-                                              </svg></a>
+                                              </svg>
+                                            </a>
                                     </b>
                                 </p>
                             </div>
@@ -54,7 +55,11 @@
 
             <div class="col-md-12 pt-5 text-center">
                 <div class="contact-btn">
-                    <a href="{{ route('front.services')}}" style="border-radius: 0px 16px 0px 16px" class="btn "><i class="las la-arrow-right"></i> {{ __('lang.view_all_services') }} </a>
+                    <a href="{{ route('front.services')}}" style="border-radius: 0px 16px 0px 16px" class="btn ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M4.5 10.9999L16.086 10.9999L11.586 6.49994L13 5.08594L19.914 11.9999L13 18.9139L11.586 17.4999L16.086 12.9999L4.5 12.9999L4.5 10.9999Z" fill="white"/>
+                          </svg>
+                        {{ __('lang.view_all_services') }} </a>
                 </div>
             </div>
         </div>
